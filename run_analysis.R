67a91a2fe78f6d68587fe368bbd4181741d5c61b
Alexander H. Prociuk <a.h.prociuk@gmail.com>
2014-09-21 14:01:48 -0400

    Added code to write out tidy data set and return it to calling frame.

--- run_analysis.R
+++ run_analysis.R
@@ -4,65 +4,69 @@ run_analysis <- function() {
     library(reshape2)
     
     ## Read in column (feature) names
-    actnmsfct <<- read.table("UCI\ HAR\ Dataset/features.txt")
-    actnms <<- as.matrix(actnmsfct)
+    actnmsfct <- read.table("UCI\ HAR\ Dataset/features.txt")
+    actnms <- as.matrix(actnmsfct)
     
     ## Read in row (activity) labels
-    rowlblsfct <<- read.table("UCI\ HAR\ Dataset/activity_labels.txt")
-    rowlbls <<- as.vector(rowlblsfct[,2])
+    rowlblsfct <- read.table("UCI\ HAR\ Dataset/activity_labels.txt")
+    rowlbls <- as.vector(rowlblsfct[,2])
     
     
     ## Read in data using actnms as column names
-    trndat <<- read.table("UCI\ HAR\ Dataset/train/X_train.txt",
+    trndat <- read.table("UCI\ HAR\ Dataset/train/X_train.txt",
                           col.names=actnms[,2])
-    testdat <<- read.table("UCI\ HAR\ Dataset/test/X_test.txt",
+    testdat <- read.table("UCI\ HAR\ Dataset/test/X_test.txt",
                           col.names=actnms[,2])
     
     
     
     ## Prune above data to include only means and 
     ## standard deviations
-    keepvc <<- c(grep("mean",actnms[,2],ignore.case=TRUE),
+    keepvc <- c(grep("mean",actnms[,2],ignore.case=TRUE),
                  grep("std",actnms[,2],ignore.case=TRUE))
-    trndat_prune <<- trndat[,keepvc]
-    testdat_prune <<- testdat[,keepvc]
+    trndat_prune <- trndat[,keepvc]
+    testdat_prune <- testdat[,keepvc]
     
     
     ## Load activity and subject vectors
-    trnlbls <<- read.table("UCI\ HAR\ Dataset/train/y_train.txt",
+    trnlbls <- read.table("UCI\ HAR\ Dataset/train/y_train.txt",
                            col.names="activity")
-    trnsbj <<- read.table("UCI\ HAR\ Dataset/train/subject_train.txt",
+    trnsbj <- read.table("UCI\ HAR\ Dataset/train/subject_train.txt",
                            col.names="subject")
-    testlbls <<- read.table("UCI\ HAR\ Dataset/test/y_test.txt",
+    testlbls <- read.table("UCI\ HAR\ Dataset/test/y_test.txt",
                            col.names="activity")
-    testsbj <<- read.table("UCI\ HAR\ Dataset/test/subject_test.txt",
+    testsbj <- read.table("UCI\ HAR\ Dataset/test/subject_test.txt",
                           col.names="subject")
     
     
     
     
     ## Relabel activity vector
-    trnlbls[,1] <<- rowlbls[trnlbls[,1]]
-    testlbls[,1] <<- rowlbls[testlbls[,1]]
+    trnlbls[,1] <- rowlbls[trnlbls[,1]]
+    testlbls[,1] <- rowlbls[testlbls[,1]]
     
     
     ## Merge activity and subject vectors with dataset
-    trnset_tot <<- cbind(trnsbj,trnlbls,trndat_prune)
-    ## trnset_tot_frame <<- data.frame(training=trnset_tot)
-    testset_tot <<- cbind(testsbj,testlbls,testdat_prune)
-    ## testset_tot_frame <<- data.frame(test=testset_tot)
+    trnset_tot <- cbind(trnsbj,trnlbls,trndat_prune)
+    ## trnset_tot_frame <- data.frame(training=trnset_tot)
+    testset_tot <- cbind(testsbj,testlbls,testdat_prune)
+    ## testset_tot_frame <- data.frame(test=testset_tot)
     
     ## Merge test an training datasets
-    allsets_tot_frame <<- rbind(trnset_tot,testset_tot)  
+    allsets_tot_frame <- rbind(trnset_tot,testset_tot)  
     
     ## Create tidy data set
-    allsets_melt <<- melt(allsets_tot_frame, 
+    allsets_melt <- melt(allsets_tot_frame, 
                           id=names(allsets_tot_frame)[1:2], 
                           measure.vars=
                             names(allsets_tot_frame)[3:length(names(allsets_tot_frame))]
                          )
-    allsets_tidy <<- dcast(allsets_melt,
+    allsets_tidy <- dcast(allsets_melt,
                            subject + activity ~ variable,
                            mean)
+    
+    write.table(allsets_tidy,file="allsets_tidy.txt",
+                row.name=FALSE, sep="\t")
+    allsets_tidy
     
 }
